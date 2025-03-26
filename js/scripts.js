9ea7c3f706ad6c3e016b806a2ae26029c666abd7
Sean McHenry <smchenrybc@gmail.com>
2025-03-26 13:02:52 -0400

    Responsive font sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-console.log(jQuery.prototype);
+$.fn.myCustomMethod = function() {
+  console.log("This is a custom jQuery method!");
+  return this; // Maintain chainability
+};
+
+// Usage:
+$(".box").myCustomMethod();
